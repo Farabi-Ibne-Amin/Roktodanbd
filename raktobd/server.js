35878fd441b9a67b4a487d727f9eaf86eb388090
Farabi Ibne Amin <farabiamin.official@gmail.com>
2026-07-15 11:28:14 +0600

    fix: remove deprecated mongoose useNewUrlParser and useUnifiedTopology options

--- raktobd/server.js
+++ raktobd/server.js
@@ -222,10 +222,7 @@ app.use((err, req, res, next) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log('✅ Connected to MongoDB');
 })
